cb66f506b9abe4a0c26cb41046b8548de520255e
Alperen Öztürk <113993783+ozturkkalperen@users.noreply.github.com>
2023-08-07 16:11:08 +0300

    Add files via upload

---
+++ w3/problems/max.c
@@ -0,0 +1,41 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int max(int array[], int n);
+
+int main(void)
+{
+    int n;
+    do
+    {
+        n = get_int("Number of elements: ");
+    } while (n < 1);
+
+    int arr[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        arr[i] = get_int("Element %i: ", i + 1);
+    }
+
+    printf("The max value is %i.\n", max(arr, n));
+}
+
+// TODO: return the max value
+int max(int array[], int n)
+{
+    int temp;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (array[j] < array[j + 1])
+            {
+                temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }
+        }
+    }
+    return array[0];
+}
